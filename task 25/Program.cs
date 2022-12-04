45a6488f870f0f32e56a73a1d594dcc88f55f8c1
lagunova1204 <lagunova.irina1204@gmail.com>
2022-12-04 15:43:16 +0300

    Задача 25

---
+++ task 25/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число, которое нужно ввести в степень");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+
+for (int i = 0; i < B; i++)
+{
+    result = result * A;
+    
+}
+
+    Console.WriteLine($"{result}");
+
