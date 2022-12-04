0e29a0f1254c9e6bb14f374613f5c0c2456dbb5d
lagunova1204 <lagunova.irina1204@gmail.com>
2022-12-04 15:53:43 +0300

    Задача 27

---
+++ task 27/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Напишите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = n; i > 0; i = i/10)
+{
+    n = i%10;
+    sum = sum + n;
+}
+Console.WriteLine($"{sum}");
