a18a12a5084cad7da59ef0805482a11608e3befb
lagunova1204 <lagunova.irina1204@gmail.com>
2022-12-04 16:04:37 +0300

    Задача 29

---
+++ task 29/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine(" Введите 8 чисел");
+int [] array = new int [8];
+for (int i = 0; i < 8; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("[{0},{1},{2},{3},{4},{5},{6},{7}]", array[0], array[1], array[2], array[3], array[4], array[5], array[6],array[7]);
